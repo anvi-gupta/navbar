e94f13ea6ab11c689e2f14e08bc57a623b394c55
anvi-gupta <69414331+anvi-gupta@users.noreply.github.com>
2020-11-28 18:51:10 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const selectElement=(element)=>document.querySelector(element);
+const header=selectElement('header');
+const mainContent=selectElement('main');
+
+selectElement('.hamburger').addEventListener('click',()=>{
+    header.classList.toggle('active');
+    mainContent.classList.toggle('active');
+});
+
+window.onclick=(event)=>{
+    if(event.target.matches('.active')){
+        if(header.classList.contains('active')){
+            header.classList.remove('active');
+            mainContent.classList.remove('active');
+        }
+    }
+};
